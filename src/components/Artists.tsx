6c5b33e03ab29cf554b3cbdb9ffa804a58eee7e9
Samuel Darbonne <samdarbonne@gmail.com>
2024-09-19 22:37:01 -0700

    cleanup

--- src/components/Artists.tsx
+++ src/components/Artists.tsx
@@ -1,19 +1,18 @@
 import { LoaderFunction, useLoaderData } from "react-router-dom";
 import api, { ArtistData, Query } from "../fetch";
+import "../styles/SongsTable.css";
+import DataTable from "./DataTable";
+import { ArtistRow } from "./ArtistRow";
 
 const fetchData = async (page: number, query: Query) => {
   return await api.artists.get.all(page, query);
 };
 
-import "../styles/SongsTable.css";
-import { ReactElement } from "react";
-import DataTable from "./DataTable";
-import { ArtistRow } from "./ArtistRow";
 export const loader: LoaderFunction = async () => {
   return await fetchData(1, "recent");
 };
 
-const Artists: () => ReactElement = () => {
+const Artists = () => {
   const initialData = useLoaderData() as ArtistData | null;
   if (!initialData) return <div>Loading...</div>;
   return (
